9af2157e0d3f3a8057dc78a77a6641fe57d4e439
Yann Leost <yannleost@gmail.com>
2018-05-22 16:28:19 +0200

    root terminée

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources course
+  get 'courses/filter',  to: 'courses#filter', as: :filter
+  resources :courses , only: [:index, :show] do
+    resources :reviews, only: [:new, :create]
+
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
