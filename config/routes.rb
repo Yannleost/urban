5c2e83eeaab5d81387a25cef51ad434044dd82a6
Yann Leost <yannleost@gmail.com>
2018-05-25 16:20:48 +0200

    Prepare for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,5 @@ Rails.application.routes.draw do
   resources :courses , only: [:index, :show] do
   resources :reviews, only: [:new, :create, :destroy]
   end
-
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
 end
