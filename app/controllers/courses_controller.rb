c4a13a26728fe71526164b0d12756138808c12c2
Yann Leost <yannleost@gmail.com>
2018-05-25 11:57:45 +0200

    ij

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,5 @@
 class CoursesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: [:index, :show, :filter]
 
   def index
     @courses = Course.all
