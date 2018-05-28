ebd0d201540430356bfd910b853acd972ee12267
Yann Leost <yannleost@gmail.com>
2018-05-28 12:00:59 +0200

    ok

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -26,20 +26,20 @@ class CoursesController < ApplicationController
 
   def search_course
     @courses = Course.all
-   raise
-    @courses = @courses.where(category: params[:category])
-    @courses = @courses.where(difficulty: params[:difficulty])
-    @courses = @courses.where(time: params[:time])
+    @courses = @courses.where(category: params[:course][:category])
+    @courses = @courses.where(difficulty: params[:course][:difficulty])
+    @courses = @courses.where(time: params[:course][:time])
+
 
     if @courses.count > 1
       @course = @courses.sample
     else
-      @courses = @course
+      @course = @courses.first
     end
     # category = params[:category]
     # difficulty = params[:difficulty]
     # time = params[:time]
-    redirect_to course_path(@course_id)
+    redirect_to course_path(@course)
     # @courses = Course.all
 end
     #
