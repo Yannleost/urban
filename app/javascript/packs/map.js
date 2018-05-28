c3a981f3642fa3be82c671af9c2f7ff2d2492d66
Yann Leost <yannleost@gmail.com>
2018-05-28 18:37:57 +0200

    fikdskl

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -6,13 +6,22 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
   const markers = JSON.parse(mapElement.dataset.markers);
   const path = JSON.parse(mapElement.dataset.path);
   map.addMarkers(markers);
-  map.drawPolyline({
-    path: path,
-    strokeColor: '#00000',
-    strokeOpacity: 0.6,
-    strokeWeight: 6
-  })
+  // map.drawPolyline({
+  //   path: path,
+  //   strokeColor: '#00000',
+  //   strokeOpacity: 0.6,
+  //   strokeWeight: 6
+  // })
+map.drawRoute({
 
+
+  origin: [43.29611, 5.36993],
+  destination: [43.28557, 5.35112],
+  travelMode: 'walking',
+  strokeColor: '#131540',
+  strokeOpacity: 0.6,
+  strokeWeight: 6
+});
   if (markers.length === 0) {
     map.setZoom(2);
   } else if (markers.length === 1) {
